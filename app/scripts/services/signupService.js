2273411645727d891545493c9f1d8e36023d8d84
edwin-rodriguez <ing.erodriguezm@gmail.com>
2016-07-10 16:47:58 -0500

    allow to save custom data to user on signup

--- app/scripts/services/signupService.js
+++ app/scripts/services/signupService.js
@@ -4,11 +4,15 @@ var URL_AUTH_API = 'http://localhost:3000/';
 
 app.factory("signupService", ["$http", "$q", function ($http, $q) {
   return {
-      signup: function (fullname, email, password) {
+      signup: function (fullname, email, password, customData) {
           var deferred = $q.defer();
 
-          $http.post(URL_AUTH_API+"signup", { fullname: fullname, email: email, password: password })
-              .then(function (result) {
+          $http.post(URL_AUTH_API+"signup", {
+            fullname: fullname,
+            email: email,
+            password: password,
+            customData: customData,
+          }).then(function (result) {
                 console.log(result.data);
                 deferred.resolve(result);
               }, function (error) {
