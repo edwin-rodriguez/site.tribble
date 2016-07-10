bec14329c0562c51d4563aa3a21f6700ae4222f8
edwin-rodriguez <ing.erodriguezm@gmail.com>
2016-07-10 16:46:46 -0500

    store repository

--- app/scripts/app.repositories.js
+++ app/scripts/app.repositories.js
@@ -2,6 +2,16 @@
 
 var URL_API = 'http://localhost:5000/';
 
+app.factory('StoreRepository', ['$resource',
+  function($resource) {
+    var resource = $resource(URL_API+'stores/:id', {
+        id:'@id'
+    }, {
+        update: {method: 'PUT'}
+    });
+    return resource;
+}]);
+
 app.factory('ArticleRepository', ['$resource',
   function($resource) {
     var resource = $resource(URL_API+'articles/:id', {
